304d9d58843b1815a63ea17e467bf33c5e722647
Ijnek117 <129587830+Ijnek117@users.noreply.github.com>
2026-03-04 16:47:25 +0000

    Depreciated Numpy function fixes and make eval run on first epoch.

--- joint_debiasing/eval_all.py
+++ joint_debiasing/eval_all.py
@@ -134,7 +134,7 @@ def run_eval(model, preprocess, attribute="gender", device=torch.device('cuda'))
     def accuracy(output, target, topk=(1,)):
         pred = output.topk(max(topk), 1, True, True)[1].t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
-        return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
+        return [correct[:k].reshape(-1).float().sum(0, keepdim=True).item() for k in topk]
 
     zeroshot_weights = zeroshot_classifier(imagenet_classes, imagenet_templates)
 
