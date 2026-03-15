03dd663a7a3ae728eb9592d1bf045161495d5321
Shreeya Gorasia <s2887183#ed.ac.uk>
2026-03-15 14:49:56 +0000

    sh file to run evaluation

---
+++ submit_new_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=clip_debias_eval_new
+#SBATCH --partition=Teaching
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --time=10:00:00
+#SBATCH --mail-user=s2142414@ed.ac.uk
+#SBATCH --mail-type=END,FAIL
+
+source ~/venv_mlp/bin/activate
+
+python -u measure_caption_bias.py \
+    --image_dir data/COCO/images/val2014 \
+    --results_filename /disk/scratch/s2142414/clip_cap_debiased.csv
